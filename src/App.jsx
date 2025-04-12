44d8b8a36a756ad60d181d2db57dd5792f5dd2e9
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-12 15:54:20 +0600

    resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
 
 function App() {
+  const bottles = [
+    { id: 1, Name: "pink nike bottle", price: 250, color: "pink" },
+    { id: 2, Name: "pink nike bottle", price: 250, color: "pink" },
+    { id: 3, Name: "pink nike bottle", price: 250, color: "pink" },
+    { id: 4, Name: "pink nike bottle", price: 250, color: "pink" },
+  ];
   return (
     <>
       <h1>Bye Awesome water bottle</h1>
