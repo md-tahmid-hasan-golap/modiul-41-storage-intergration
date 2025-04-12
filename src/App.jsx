6b78aa0ed99b3ccb4021d6fcdbf47974407f12c4
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-12 14:43:32 +0600

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { add, diff, division } from "./utility/math/math";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const sum = add(10, 20);
-  const subs = diff(20, 15);
-  const divi = division(100, 50);
-
-  console.log(sum);
-  console.log(subs);
-  console.log(divi);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bye Awesome water bottle</h1>
     </>
   );
 }
--- src/utility/math/math.js
+++
@@ -1,6 +0,0 @@
-const add = (a, b) => a + b;
-
-const diff = (a,b) => a - b;
-const division = (a, b) => a / b
-
-export {add, diff, division}
